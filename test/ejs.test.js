52fc34800d7dabb4e4040593b758e1a7481ec05a
Haoliang Gao <sakura9515@gmail.com>
2017-02-17 11:08:33 +0800

    feat: first implement (#1)

---
+++ test/ejs.test.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const path = require('path');
+const request = require('supertest');
+const mm = require('egg-mock');
+const fs = require('mz/fs');
+
+const fixtures = path.join(__dirname, 'fixtures');
+
+
+describe('test/egg-view-ejs.test.js', () => {
+
+  describe('render', () => {
+    let app;
+    before(() => {
+      app = mm.app({
+        baseDir: 'apps/ejs-view',
+      });
+      return app.ready();
+    });
+    after(() => app.close());
+
+    it('should render with locals', () => {
+      return request(app.callback())
+        .get('/locals')
+        .expect('hello world\n')
+        .expect(200);
+    });
+
+    it('should render with include', () => {
+      return request(app.callback())
+        .get('/include')
+        .expect('hello header\nhello footer\n')
+        .expect(200);
+    });
+
+    it('should render with helper', () => {
+      return request(app.callback())
+        .get('/helper')
+        .expect('hello world\n')
+        .expect(200);
+    });
+
+    it('should render with cache', function* () {
+      const cacheFile = path.join(fixtures, 'apps/ejs-view/app/view/cache.ejs');
+      yield fs.writeFile(cacheFile, '1');
+      yield request(app.callback())
+        .get('/cache')
+        .expect('1')
+        .expect(200);
+
+      yield fs.writeFile(cacheFile, '2');
+      yield request(app.callback())
+        .get('/cache')
+        .expect('1')
+        .expect(200);
+    });
+
+    it('should render with html extension', () => {
+      return request(app.callback())
+        .get('/htmlext')
+        .expect('hello world\n')
+        .expect(200);
+    });
+
+    it('should render error', () => {
+      return request(app.callback())
+        .get('/error')
+        .expect('Could not find matching close tag for "<%".')
+        .expect(200);
+    });
+  });
+
+  describe('renderString', () => {
+    let app;
+    before(() => {
+      app = mm.app({
+        baseDir: 'apps/ejs-view',
+      });
+      return app.ready();
+    });
+    after(() => app.close());
+
+    it('should renderString with data', () => {
+      return request(app.callback())
+        .get('/render-string')
+        .expect('hello world')
+        .expect(200);
+    });
+
+    it('should renderString with helper', () => {
+      return request(app.callback())
+        .get('/render-string-helper')
+        .expect('hello world')
+        .expect(200);
+    });
+
+    it('should renderString error', () => {
+      return request(app.callback())
+        .get('/render-string-error')
+        .expect('Could not find matching close tag for "<%".')
+        .expect(200);
+    });
+  });
+
+  describe('no cache', () => {
+    let app;
+    before(() => {
+      mm.env('local');
+      app = mm.app({
+        baseDir: 'apps/ejs-view',
+      });
+      return app.ready();
+    });
+    after(() => app.close());
+
+    it('should render without cache', function* () {
+      const cacheFile = path.join(fixtures, 'apps/ejs-view/app/view/cache.ejs');
+      yield fs.writeFile(cacheFile, '1');
+      yield request(app.callback())
+        .get('/cache')
+        .expect('1')
+        .expect(200);
+
+      yield fs.writeFile(cacheFile, '2');
+      yield request(app.callback())
+        .get('/cache')
+        .expect('2')
+        .expect(200);
+    });
+  });
+});
---
+++ test/fixtures/apps/ejs-view/app/controller/view.js
@@ -0,0 +1,49 @@
+'use strict';
+
+exports.renderWithLocals = function* (ctx) {
+  yield ctx.render('locals.ejs', {
+    data: 'world',
+  });
+};
+
+exports.include = function* (ctx) {
+  yield ctx.render('include/index.ejs');
+};
+
+exports.renderWithHelper = function* (ctx) {
+  yield ctx.render('helper.ejs');
+};
+
+exports.cache = function* (ctx) {
+  yield ctx.render('cache.ejs');
+};
+
+exports.htmlext = function* (ctx) {
+  yield ctx.render('ejs.html');
+};
+
+exports.error = function* (ctx) {
+  try {
+    yield ctx.render('error.ejs');
+  } catch (err) {
+    this.body = err.message;
+  }
+};
+
+exports.renderStringWithData = function* (ctx) {
+  ctx.body = yield ctx.renderString('hello <%= data %>', {
+    data: 'world',
+  });
+};
+
+exports.renderStringWithHelper = function* (ctx) {
+  ctx.body = yield ctx.renderString('hello <%= helper.data() %>');
+};
+
+exports.renderStringError = function* (ctx) {
+  try {
+    yield ctx.renderString('<% a');
+  } catch (err) {
+    ctx.body = err.message;
+  }
+};
